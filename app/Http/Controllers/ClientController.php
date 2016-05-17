a731f8045f85ee78834e6e8cabfd342eb7c43656
Fabio Pimenta <fabio@ispti.com.br>
2016-05-17 09:21:49 -0300

    correcao fase 1

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,10 +22,32 @@ class ClientController extends Controller
     }
 
     public function update(Request $request, $id){
-        Client::find($id)->update($request->all());
+        try {
+            $cli = Client::find($id);
+            if (is_null($cli)) {
+                return json_encode(["message" => "Id incorreto"]);
+            }
+            if ($cli->update($request->all())) {
+                return json_encode(["message" => "Client {$cli['original']['name']} foi atualizado"]);
+            }
+        } catch (\Exception $e){
+            return json_encode(["message" => "Erro deletando Client"]);
+        }
     }
 
-    public function destroy($id){
-        Client::find($id)->delete();
+    public function destroy($id)
+    {
+        try {
+            $cli = Client::find($id);
+            if (is_null($cli)){
+                return json_encode(["message" => "Id incorreto"]);
+            }
+            $mess = $cli['name'];
+            if ($cli->delete()) {
+                return json_encode(["message" => "Client {$mess} foi removido"]);
+            }
+        } catch (\Exception $e){
+            return json_encode(["message" => "Erro deletando Client"]);
+        }
     }
 }
