4c95abe307597a22e46d5878dc43e67dd263c967
Fabio Pimenta <efepimenta@gmail.com>
2016-06-22 22:54:41 -0300

    22-06-2016 - home

--- resources/assets/js/services/project.task.js
+++ resources/assets/js/services/project.task.js
@@ -5,12 +5,13 @@ angular.module('app.services')
             function dateToBr(data, headers) {
                 var o = appConfig.utils.transformResponse(data, headers);
                 if (angular.isObject(o)) {
+                    var arr_data = {};
                     if (o.hasOwnProperty('due_date')) {
-                        var arr_data = o.due_date.split('-');
+                        arr_data = o.due_date.split('-');
                         o.due_date = $filter('date')(new Date(arr_data[0], arr_data[1] - 1, arr_data[2]), 'dd/MM/yyyy');
                     }
                     if (o.hasOwnProperty('start_date')) {
-                        var arr_data = o.start_date.split('-');
+                        arr_data = o.start_date.split('-');
                         o.start_date = $filter('date')(new Date(arr_data[0], arr_data[1] - 1, arr_data[2]), 'dd/MM/yyyy');
                     }
                 }
